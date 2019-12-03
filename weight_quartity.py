6485bc6c93ce5c96fe7ec84c3b31798c677d00aa
zhanghuashine <44055480+zhanghuashine@users.noreply.github.com>
2019-12-03 16:37:26 +0800

    Update weight_quartity.py

--- weight_quartity.py
+++ weight_quartity.py
@@ -1,3 +1,30 @@
+def weight_quantize(index, activation):
+
+    tensor_distubution = np.zeros(INTERVAL_NUM)
+    print("\nQuantize the Layer{}_Weight:".format(index))
+
+    max_val = np.max(activation)
+    min_val = np.min(activation)
+    tensor_max = max(abs(max_val), abs(min_val))
+    distubution_interval = STATISTIC * tensor_max / INTERVAL_NUM
+
+    th = tensor_max
+    hist, hist_edge = np.histogram(activation, bins=INTERVAL_NUM, range=(0, th))  # ?
+    tensor_distubution += hist
+
+    distribution = np.array(tensor_distubution)
+
+    threshold_bin = threshold_distribution(distribution)  # 作用？（比较耗时）
+
+    threshold = (threshold_bin + 0.5) * distubution_interval
+
+    activation_scale = QUANTIZE_NUM / threshold
+    # print("%-8d threshold : %-10f interval : %-10f scale : %-10f" % (
+    # threshold_bin, threshold, distubution_interval, activation_scale))
+
+    return activation_scale
+
+
 def threshold_distribution(distribution, target_bin=128):
     distribution = distribution[1:]
     length = distribution.size
